ca123f02e8295bd4a4b388d8597f3f6a696fc4b7
Arpita Das <dasarpita0369@gmail.com>
2026-06-21 19:13:35 +0530

    Update ExecuteChaseCaseStudy.java

--- ExecuteChaseCaseStudy.java
+++ ExecuteChaseCaseStudy.java
@@ -1,5 +1,20 @@
 package com.ExecuteChaseCasestudy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.ChasePage.LoginChasePage;
+import com.Helper.HelperFunction;
 
 public class ExecuteChaseCaseStudy {
+	@Test
+	public void ValidateUser() {
+		WebDriver driver = HelperFunction.HelperOptions("chrome", "https://www.chase.com/");
+		LoginChasePage login = PageFactory.initElements(driver, LoginChasePage.class);
+		login.LoginChase("devanshbajpai07@gmail.com", "Devansh34");
+		
+	}
+
+}
 
 }
