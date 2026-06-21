758df997127201477c4d6be28cfaab099b19a851
Arpita Das <dasarpita0369@gmail.com>
2026-06-21 19:14:23 +0530

    Update BrowserFactory.java

--- BrowserFactory.java
+++ BrowserFactory.java
@@ -1,5 +1,21 @@
 package com.Helper;
-
+  import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 public class BrowserFactory {
+	static WebDriver driver;
+	
+	public static WebDriver HelperOptions(String browserName, String URL) {
+		if(browserName.equalsIgnoreCase("Google Chrome") || browserName.equalsIgnoreCase("GC") || browserName.equalsIgnoreCase("Chrome")) {
+			driver = new ChromeDriver();
+		}else if(browserName.equalsIgnoreCase("Microsoft Edge") || browserName.equalsIgnoreCase("ED") || browserName.equalsIgnoreCase("Edge")) {
+			
+			driver = new EdgeDriver();
+		}else if(browserName.equalsIgnoreCase("Microsoft Edge") || browserName.equalsIgnoreCase("ED") || browserName.equalsIgnoreCase("Edge")) {
+			driver= new FirefoxDriver();
+		}
+		
+		driver.manage().window().max
 
 }
